c5332e87fd8c564b06b831c016f50780b0b5fdf5
[user_name] <[user_email_address]>
2022-08-02 22:20:22 +0900

    first

---
+++ app/Http/Controllers/GreetingController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GreetingController extends Controller
+{
+    public function index()
+    {
+        return view('greeting.index');
+    }
+
+    public function welcome(Request $request)
+    {
+        $username = $request->input('username');
+        // chenge
+        return view('greeting.welcome', ['username' => $username]);
+    }
+}
